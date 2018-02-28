1adc637725491d70adfb0f366da70138475b8002
mitchainslieit <35722802+mitchainslieit@users.noreply.github.com>
2018-02-28 12:27:52 +0800

    v2

--- Sockets2/ClientS.java
+++ Sockets2/ClientS.java
@@ -1,24 +1,32 @@
 import java.io.*;
 import java.net.*;
 
+/**
+ * Description: This program allows both server and client to chat simultaneously using sockets.
+ * @author GALATCHA, Mitch Ainslie V.
+ * @author SALAZAR, Jerome Francis N.
+ * @version 1.0, February 28, 2018
+ */
+
 public class  ClientS{
 
     public static Socket sock;
-    public static BufferedReader read1, receive2;
+    public static BufferedReader read, receive2;
     public static String in = "", out = "";
     public static PrintWriter print;
 
 	public static void main(String args[]) {
 		try {
 			sock = new Socket("127.0.0.1", 4040);
-			read1 = new BufferedReader(new InputStreamReader(System.in));
+			read = new BufferedReader(new InputStreamReader(System.in));
 			receive2 = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 			print = new PrintWriter(sock.getOutputStream(), true);
 			while (true) {
 				System.out.print("Enter your chat: ");
-				in = read1.readLine();
+				in = read.readLine();
 				print.println(in);
 				print.flush();
+				System.out.println("Waiting for reply...");
 				out = receive2.readLine();
 				if (out!= null) {
 					System.out.println("Server: " + out);
--- Sockets2/ServerS.java
+++ Sockets2/ServerS.java
@@ -1,16 +1,23 @@
+/**
+ * Description: This program allows both server and client to chat simultaneously using sockets.
+ * @author GALATCHA, Mitch Ainslie V.
+ * @author SALAZAR, Jerome Francis N.
+ * @version 1.0, February 28, 2018
+ */
+
 import java.io.*;
 import java.net.*;
 
 public class ServerS {
 
     public static Socket sock;
-    public static BufferedReader read1, receive2;
+    public static BufferedReader read, receive2;
     public static String in = "", out = "";
     public static PrintWriter print;
 
 	public static void main(String args[]) {
 		try {
-			read1 = new BufferedReader(new InputStreamReader(System.in));
+			read = new BufferedReader(new InputStreamReader(System.in));
 			ServerSocket newSocket = new ServerSocket(4040);
 			System.out.println("Waiting for the Client ...");
 			sock = newSocket.accept();
@@ -23,9 +30,10 @@ public class ServerS {
 					System.out.println("Client: " + out);
 				}
 				System.out.print("Enter your chat: ");
-				in = read1.readLine();
+				in = read.readLine();
 				print.println(in);
 				print.flush();
+				System.out.println("Waiting for reply...");
 			}
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
